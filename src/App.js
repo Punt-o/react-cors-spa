32065db7394c22f83317b117fe83914ccb4a55d8
Mario Càllisto <mario.callisto@gmail.com>
2024-06-29 10:10:39 +0200

    Closer to AWS template

--- src/App.js
+++ src/App.js
@@ -1,15 +1,34 @@
 import React from 'react';
 import './App.css';
 import MatchStatistics from './MatchStatistics';
+import { useFetch } from "react-async"
+
+// To be replaced by the endpoint of the API deployed through the CloudFormation Template
+const APIEndPoint = 'to be replaced with your api endpoint here'
 
 function App() {
-    const apiUrl = 'http://localhost:8000/padel_match_statistics.json'; // URL locale del file JSON
+    // const apiUrl = 'http://localhost:8000/padel_match_statistics.json'; // URL locale del file JSON
 
     return (
         <div className="App">
-            <MatchStatistics apiUrl={apiUrl} />
+            {/*<header className="App-header">*/}
+            {/*    {APIEndPoint.startsWith('http') &&*/}
+            {/*        <APIResult />*/}
+            {/*    }*/}
+            {/*</header>*/}
+            <MatchStatistics />
+            {/*<MatchStatistics apiUrl={apiUrl} />*/}
         </div>
     );
+}
+
+const APIResult = () => {
+    const { data, error } = useFetch(APIEndPoint, {
+        headers: { accept: "application/json" },
+    })
+    if (error) return <p>{error.message}</p>
+    if (data) return <p>{data.message}</p>
+    return null
 }
 
 export default App;
